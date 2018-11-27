fe1a9db3154fb23fab8555d25e38d8ffee20830b
MiltonLn <miltonln04@gmail.com>
2018-11-27 15:03:48 -0500

    Changing WebsocketConsumer by JsonWebsocketConsumer

--- channels2_todo_list/lists/consumers.py
+++ channels2_todo_list/lists/consumers.py
@@ -1,12 +1,10 @@
-import json
-
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
 from .models import TODOList, Task
 
 
-class TODOListConsumer(WebsocketConsumer):
+class TODOListConsumer(JsonWebsocketConsumer):
     def connect(self):
         list_pk = self.scope['url_route']['kwargs']['list_pk']
         self.list = TODOList.objects.get(id=list_pk)
@@ -24,19 +22,18 @@ class TODOListConsumer(WebsocketConsumer):
             self.channel_name
         )
 
-    def receive(self, text_data):
-        data = json.loads(text_data)
-        msg_type = data.get('type', None)
+    def receive_json(self, content):
+        msg_type = content.get('type', None)
 
         if msg_type == 'create_task':
-            self.create_task(data)
+            self.create_task(content)
         elif msg_type == 'update_task':
-            self.update_task(data)
+            self.update_task(content)
         else:
             raise Exception(f'Method {msg_type} not supported')
 
-    def create_task(self, data):
-        description = data['description']
+    def create_task(self, content):
+        description = content['description']
         task = self.list.task_set.create(description=description)
         async_to_sync(self.channel_layer.group_send)(
             self.list_group_name,
@@ -47,9 +44,9 @@ class TODOListConsumer(WebsocketConsumer):
             }
         )
 
-    def update_task(self, data):
-        uuid = data['uuid']
-        value = data['value']
+    def update_task(self, content):
+        uuid = content['uuid']
+        value = content['value']
         self.list.task_set.filter(id=uuid).update(completed=value)
         async_to_sync(self.channel_layer.group_send)(
             self.list_group_name,
@@ -61,15 +58,15 @@ class TODOListConsumer(WebsocketConsumer):
         )
 
     def toggle_task(self, event):
-        self.send(text_data=json.dumps({
+        self.send_json({
             'type': 'toggle_task',
             'id': event['id'],
             'value': event['value']
-        }))
+        })
 
     def refresh_list(self, event):
-        self.send(text_data=json.dumps({
+        self.send_json({
             'type': 'refresh_list',
             'id': event['id'],
             'description': event['description']
-        }))
+        })
